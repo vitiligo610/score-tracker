71185384000e5d3c050924269ad3e8c0613f0feb
Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>
2024-12-12 20:22:02 +0500

    add route files
    Signed-off-by: Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>

---
+++ src/app/series/page.tsx
@@ -0,0 +1,11 @@
+const Series = () => {
+  return (
+    <div className="container mx-auto py-8 space-y-8">
+      <div className="w-full flex justify-between items-center mb-12">
+        <h1 className="text-7xl text-primary font-bold">Series</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Series;
