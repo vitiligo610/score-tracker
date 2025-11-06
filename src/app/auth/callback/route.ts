851e652df6e22c778e813b456ae3312f14d01036
Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>
2025-11-06 21:10:36 +0500

    add base url to workos redirect
    Signed-off-by: Ahmed Javed <88615893+vitiligo610@users.noreply.github.com>

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,3 +1,6 @@
 import { handleAuth } from "@workos-inc/authkit-nextjs";
 
-export const GET = handleAuth({ returnPathname: "/home" });
+export const GET = handleAuth({
+  baseURL: process.env.NEXT_PUBLIC_APP_URL,
+  returnPathname: "/home"
+});
